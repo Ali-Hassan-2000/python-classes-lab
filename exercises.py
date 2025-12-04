cf029a4bc692e26b12b8a11a2cf4dd714f4a2a04
ali <ali.hassan.salman.2000@gmail.com>
2025-12-04 13:47:12 +0300

    Step 3

--- exercises.py
+++ exercises.py
@@ -11,10 +11,36 @@ class Game:
             'a3': None, 'b3': None, 'c3': None,
         }
     
+
     def play_game(self):
         print("🎮 Welcome to Tic-Tac-Toe! The game is starting...")
+
+    def print_board(self):
+        b = self.board
+        print(f"""
+              A   B   C
+          1)  {b['a1'] or ' '} | {b['b1'] or ' '} | {b['c1'] or ' '}
+              ----------
+          2)  {b['a2'] or ' '} | {b['b2'] or ' '} | {b['c2'] or ' '}
+              ----------
+          3)  {b['a3'] or ' '} | {b['b3'] or ' '} | {b['c3'] or ' '}
+        """)
+
+    def print_message(self):
+        if self.tie:
+            print("Tie game!")
+        elif self.winner:
+            print(f"{self.winner} wins the game!")
+        else:
+            print(f"It's player {self.turn}'s turn!")
+
+    def render(self):
+        self.print_board()
+        self.print_message()
 
 
 player1 = Game()
 
 player1.play_game()
+player1.render()
+
