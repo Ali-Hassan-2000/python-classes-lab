2c276a39f274009b22c94a1166b1773fdc27da48
ali <ali.hassan.salman.2000@gmail.com>
2025-12-04 13:33:13 +0300

    Step 2

--- exercises.py
+++ exercises.py
@@ -10,4 +10,11 @@ class Game:
             'a2': None, 'b2': None, 'c2': None,
             'a3': None, 'b3': None, 'c3': None,
         }
+    
+    def play_game(self):
+        print("🎮 Welcome to Tic-Tac-Toe! The game is starting...")
 
+
+player1 = Game()
+
+player1.play_game()
